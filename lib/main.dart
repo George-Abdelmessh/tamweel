99f2f0d1df301ca6beccf5c347bae00df87e84c2
Youssef Zakaria <youssef.zakaria.dev@gmail.com>
2022-08-15 11:42:06 +0200

    Locales support

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:tamweel/layout/home/home_screen.dart';
+import 'package:tamweel/shared/style/app_helper.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:tamweel/shared/style/locales.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(EasyLocalization(
+      supportedLocales: AppLocales.supportedLocales,
+      path:
+          'assets/translations', // <-- change the path of the translation files
+      fallbackLocale: AppLocales.supportedLocales[0],
+      child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return GestureDetector(
+      onTap: () {
+        AppHelper.closeKeyboard();
+      },
+      child: MaterialApp(
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomeScreen(),
       ),
-      home:  const HomeScreen(),
     );
   }
 }
