0530702367e1c1bd3fce2c1559ebf873cc1faedf
Youssef Zakaria <youssef.zakaria.dev@gmail.com>
2022-08-16 14:35:56 +0200

    auth Providers

---
+++ lib/models/auth/app_user_model.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+enum authState {
+  guest,
+  loggedIn,
+}
+
+@immutable
+class AppUser {
+  ///Immutable Constructor
+  const AppUser(
+      {this.accessToken,
+      this.refreshToken,
+      this.userId,
+      required this.userState});
+
+  /// The user's Access Token used for Requests.
+  final String? accessToken;
+
+  /// The user's Refresh Token used to get a new AccessToken.
+  final String? refreshToken;
+
+  /// The user's unique ID.
+  final String? userId;
+
+  /// The User's Authentication state
+  final authState userState;
+
+  /// Returns a copy of the current AppUser with the given values.
+  AppUser copyWith({
+    String? accessToken,
+    String? refreshToken,
+    String? userId,
+    required authState userState,
+  }) {
+    return AppUser(
+      accessToken: accessToken ?? this.accessToken,
+      refreshToken: refreshToken ?? this.refreshToken,
+      userId: userId ?? this.userId,
+      userState: this.userState,
+    );
+  }
+}
---
+++ lib/providers/auth/app_user_provider.dart
@@ -0,0 +1,36 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:tamweel/models/auth/app_user_model.dart';
+
+///Auth Notifier that allows UI to login, logout, and access user tokens.
+class AuthNotifier extends StateNotifier<AppUser> {
+  AuthNotifier() : super(AppUser(userState: authState.guest));
+
+  /// Login the user with the given email and password.
+  Future<void> login(String email, String password) async {
+    //TODO: Login the user using Auth repository.
+    try {
+      await Future.delayed(Duration(seconds: 1));
+      state = state.copyWith(
+        userState: authState.loggedIn,
+        accessToken: 'accessToken',
+        refreshToken: 'refreshToken',
+        userId: 'userId',
+      );
+    } catch (e) {
+      state = AppUser(userState: authState.guest);
+    }
+  }
+
+  /// Logout the user.
+  /// Clears all tokens and sets the user state to guest.
+  void logout() async {
+    try {
+      await Future.delayed(Duration(seconds: 1));
+      state = AppUser(userState: authState.guest);
+    } catch (e) {}
+  }
+}
+
+/// Provider that provides the AuthNotifier.
+/// This is used to notify the UI of the user's authentication state.
+final authNotifierProvider = StateNotifierProvider((ref) => AuthNotifier());
