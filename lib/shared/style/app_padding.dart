2fcd58d68da7d946ff03b87291e85c35988ec50c
George AbdElmesseh <76843136+George-Abdelmessh@users.noreply.github.com>
2022-08-21 10:06:13 +0200

    add padding 005

--- lib/shared/style/app_padding.dart
+++ lib/shared/style/app_padding.dart
@@ -13,22 +13,31 @@ class AppPadding {
   static EdgeInsets padding20 = EdgeInsets.all(AppSize.width * 0.018);
 
   /// 0 10 0 10
-  static EdgeInsets paddingH10 = EdgeInsets.symmetric(horizontal: AppSize.width * 0.01);
+  static EdgeInsets paddingH10 =
+      EdgeInsets.symmetric(horizontal: AppSize.width * 0.01);
 
   /// 0 15 0 15
-  static EdgeInsets paddingH15 = EdgeInsets.symmetric(horizontal: AppSize.width * 0.014);
+  static EdgeInsets paddingH15 =
+      EdgeInsets.symmetric(horizontal: AppSize.width * 0.014);
 
   /// 0 20 0 20
-  static EdgeInsets paddingH20 = EdgeInsets.symmetric(horizontal: AppSize.width * 0.018);
+  static EdgeInsets paddingH20 =
+      EdgeInsets.symmetric(horizontal: AppSize.width * 0.018);
+
+  static EdgeInsets paddingH005 =
+      EdgeInsets.symmetric(horizontal: AppSize.width * 0.05);
 
   /// 10 0 10 0
-  static EdgeInsets paddingV10 = EdgeInsets.symmetric(vertical: AppSize.width * 0.01);
+  static EdgeInsets paddingV10 =
+      EdgeInsets.symmetric(vertical: AppSize.width * 0.01);
 
   /// 0 15 0 15
-  static EdgeInsets paddingV15 = EdgeInsets.symmetric(vertical: AppSize.width * 0.014);
+  static EdgeInsets paddingV15 =
+      EdgeInsets.symmetric(vertical: AppSize.width * 0.014);
 
   /// 20 0 20 0
-  static EdgeInsets paddingV20 = EdgeInsets.symmetric(vertical: AppSize.width * 0.018);
+  static EdgeInsets paddingV20 =
+      EdgeInsets.symmetric(vertical: AppSize.width * 0.018);
 
   /// PADDING FOR TEXT FORM FIELD 20 5 20 5
   static EdgeInsets paddingTextFormField = EdgeInsets.symmetric(
