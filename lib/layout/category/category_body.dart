c668c7bab6effd2c38c6315df9ebf10d9cb990b2
Kariimreda <67121483+Kariimreda@users.noreply.github.com>
2022-08-24 15:47:57 +0200

    loadingCategoryScreen

--- lib/layout/category/category_body.dart
+++ lib/layout/category/category_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tamweel/layout/category/widget/category_screen.dart';
 import 'package:tamweel/providers/category/loan_provider.dart';
+import 'package:tamweel/shared/custom_widgets/custom_hud.dart';
 import 'package:tamweel/shared/style/app_color.dart';
 
 class CategoryBody extends ConsumerWidget {
@@ -10,21 +11,19 @@ class CategoryBody extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final loanProvider = ref.watch(LoanFutureProvider);
-    return Scaffold(
-      backgroundColor: AppColor.grey,
-      body: loanProvider.when(
-          data: (data) =>
-          CategoryScreen(loandata: data),
-          error: (error, stack) => Container(
-                color: AppColor.error,
-                width: 100,
-                height: 100,
-              ),
-          loading: () => Container(
-                color: AppColor.secondary,
-                width: 100,
-                height: 100,
-              )),
+    return CustomHudWidget(
+      child: Scaffold(
+        backgroundColor: AppColor.grey,
+        body: loanProvider.when(
+            data: (data) =>
+            CategoryScreen(loandata: data),
+            error: (error, stack) => Container(
+                  color: AppColor.error,
+                  width: 100,
+                  height: 100,
+                ),
+            loading: () => SizedBox.shrink()),
+      ),
     );
   }
 }
