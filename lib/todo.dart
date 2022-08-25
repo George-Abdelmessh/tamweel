9252b102070d30d85edda275d9d3262a1cf3384b
Youssef Zakaria <youssef.zakaria.dev@gmail.com>
2022-08-25 10:49:17 +0200

    todo List init

---
+++ lib/todo.dart
@@ -0,0 +1,28 @@
+// $ This file is a simple Todo list for:
+  /// - [abc] a change to an existing feature   >- color code ! red
+  /// - [abc] missing functionality             >- color code * green
+  /// - [abc] missing documentation             >- color code ? blue
+  /// - [abc] needed refactoring                >- color code ? blue
+  /// - and so On ...
+  /// [TODO: abc] Current Work in Progress/Highest Priority
+  /// 
+
+// $ Extension Name for Coloring Comments: Better Comments
+// ? The purple color '$' can be added to the extension in settings: #6C30D3.
+// feel free to add as many colors as you want in your own extension,
+// and your own todo list. 
+
+//TODO Zaka: > Making the Todo list
+// $ todo list:
+// ! [Error Messages on Dio Response] missing functionality
+// ! [Login Api Call] missing functionality
+// ? [Auth Flow] Documentation
+// * [Application Screen] new feature
+
+
+//Todo Kariim: > Adding to the Todo List
+// $ todo list:
+
+
+//Todo George: > Adding to the Todo List
+// $ todo list:
