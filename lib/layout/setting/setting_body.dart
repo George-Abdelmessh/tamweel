9709d3aeeaa6f3a29312dff4759a1d9150fca0e9
Kariimreda <67121483+Kariimreda@users.noreply.github.com>
2022-08-28 14:51:39 +0200

    container screen

--- lib/layout/setting/setting_body.dart
+++ lib/layout/setting/setting_body.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
