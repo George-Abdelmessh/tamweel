4f3fb277e3a20930ecc73fabcf181cbe4a78946a
Youssef Zakaria <youssef.zakaria.dev@gmail.com>
2022-08-29 11:05:04 +0200

    token and apply for loan init

---
+++ lib/layout/apply/form_QA_widgets/string_string.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:tamweel/shared/constants/app_constants.dart';
+import 'package:tamweel/shared/custom_widgets/custom_text_form_with_validator.dart';
+import 'package:tamweel/shared/style/app_color.dart';
+import 'package:tamweel/shared/style/app_padding.dart';
+
+class QAStringStringOneLine extends HookConsumerWidget {
+  const QAStringStringOneLine({
+    super.key,
+    this.title,
+    this.hint,
+    required this.step,
+    required this.index,
+  });
+  final String? title;
+  final String? hint;
+  final int step;
+  final int index;
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final answerController = useTextEditingController();
+
+    return Padding(
+      padding: AppPadding.paddingH005,
+      child: SizedBox(
+        width: AppSize.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //Text with title as question
+            if (title != null) Text(title!),
+            SizedBox(
+              height: AppSize.height * 0.01,
+            ),
+            customTextFormFieldWithValidator(
+              context,
+              hintText: hint,
+              controller: answerController,
+              onChanged: (value) {
+                //TODO: Set Provider.Answers at [step] and [index] to the value
+              },
+              maxLines: 1,
+              borderColor: AppColor.secondary,
+              keyboardType: TextInputType.text,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/layout/auth/login_screen.dart
+++ lib/layout/auth/login_screen.dart
@@ -5,7 +5,6 @@ import 'package:tamweel/layout/auth/widgets/login/login_screen_form.dart';
 import 'package:tamweel/layout/auth/widgets/login/login_screen_pre_form.dart';
 import 'package:tamweel/layout/home/home_screen.dart';
 import 'package:tamweel/providers/auth/app_user_provider.dart';
-import 'package:tamweel/providers/auth/user_details_provider.dart';
 import 'package:tamweel/shared/constants/app_constants.dart';
 import 'package:tamweel/shared/custom_widgets/Hud/custom_spin_hud.dart';
 import 'package:tamweel/shared/custom_widgets/custom_floating_back_button.dart';
@@ -32,10 +31,10 @@ class LoginScreen extends HookConsumerWidget {
         await authProvider
             .login(emailController.text, passwordController.text)
             .then((value) {
-          if (value.item1) {
-            print(
-              'user details: \nName: ${ref.read(userDetailsProvider.notifier).state!.name},\nEmail: ${ref.read(userDetailsProvider.notifier).state!.email}',
-            );
+          if (value) {
+            // print(
+            //   'user details: \nName: ${ref.read(userDetailsProvider.notifier).state!.name},\nEmail: ${ref.read(userDetailsProvider.notifier).state!.email}',
+            // );
 
             CacheHelper.saveData(key: 'email', value: emailController.text);
             CacheHelper.saveData(
