ddceeda47fbb8090b4fec2010568143ffe74a01d
George AbdElmesseh <76843136+George-Abdelmessh@users.noreply.github.com>
2022-08-31 15:08:49 +0200

    apply loan
    handle switch case

--- lib/providers/apply/form_widgets_provider.dart
+++ lib/providers/apply/form_widgets_provider.dart
@@ -36,17 +36,74 @@ class FormWidgets extends ConsumerWidget {
           itemCount: map.length,
           itemBuilder: (context, index) {
             //TODO: return items = genereate widgets of [map] method
-            return Container(
-              color: Color.fromRGBO(
-                Random().nextInt(100) + 150,
-                Random().nextInt(100) + 150,
-                Random().nextInt(100) + 150,
-                1.0,
-              ),
-              width: AppSize.width,
-              height: Random().nextDouble() * 200 + 25,
-              child: Center(child: Text(index.toString())),
-            );
+            switch (map[index]['type']) {
+              case 1:
+                return Container();
+              case 2:
+                return Container();
+              case 3:
+                return Container();
+              case 4:
+                return Container();
+              case 5:
+                return Container();
+              case 6:
+                return Container();
+              case 7:
+                return Container();
+              case 8:
+                return Container();
+              case 9:
+                return Container();
+              case 10:
+                return Container();
+              case 11:
+                return Container();
+              case 12:
+                return Container();
+              case 13:
+                return Container();
+              case 14:
+                return Container();
+              case 15:
+                return Container();
+              case 16:
+                return Container();
+              case 17:
+                return Container();
+              case 18:
+                return Container();
+              case 19:
+                return Container();
+              case 20:
+                return Container();
+              case 21:
+                return Container();
+              case 23:
+                return Container();
+              case 24:
+                return Container();
+              case 25:
+                return Container();
+              case 26:
+                return Container();
+              case 27:
+                return Container();
+              default:
+                return Container();
+            }
+
+            // return Container(
+            //   color: Color.fromRGBO(
+            //     Random().nextInt(100) + 150,
+            //     Random().nextInt(100) + 150,
+            //     Random().nextInt(100) + 150,
+            //     1.0,
+            //   ),
+            //   width: AppSize.width,
+            //   height: Random().nextDouble() * 200 + 25,
+            //   child: Center(child: Text(index.toString())),
+            // );
           },
         ),
       ),
